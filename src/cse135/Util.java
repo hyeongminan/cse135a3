52ae66b64be1e86b618323e4dd9aa21a3d7c07b3
ehdee <ehdee@eddies-mbp>
2014-06-05 15:49:17 -0700

    Util.java file permanent

--- src/cse135/Util.java
+++ src/cse135/Util.java
@@ -13,8 +13,8 @@ public class Util {
 	public static final String PORTNUMBER = "5432";
 	*/
 	public static final String SERVERNAME = "localhost";
-	public static final String USERNAME = "ehdee";
-	public static final String PASSWORD = "ehdee";
+	public static final String USERNAME = "postgres";
+	public static final String PASSWORD = "postgres";
 	public static final String DATABASE = "p1";
 	public static final String PORTNUMBER = "5432";
 	
