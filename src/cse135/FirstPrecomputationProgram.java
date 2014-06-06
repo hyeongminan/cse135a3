a55db2e2dd7267595a1e2aafc398c9975359cbe6
ehdee <ehdee@eddies-mbp>
2014-06-06 16:48:50 -0700

    Completed First Precomputation Program file

--- src/cse135/FirstPrecomputationProgram.java
+++ src/cse135/FirstPrecomputationProgram.java
@@ -33,14 +33,41 @@ public class FirstPrecomputationProgram {
 					+ "from users u, sales s, products p, categories c "
 					+ "where u.id = s.uid and s.pid = p.id and p.cid = c.id "
 					+ "group by u.id, c.id;;";
-			
 			stmt.execute(SQL);
 			
+			SQL = "insert into p_customer_state "
+					+ "select u.id as uid, u.state, sum(s.price * s.quantity) as total, u.name "
+					+ "from users u, sales s "
+					+ "where u.id = s.uid "
+					+ "group by u.id;";
+			stmt.execute(SQL);
 			
+			SQL = "insert into p_category_state "
+					+ "select c.id as cid, u.state, sum(s.price * s.quantity) as total "
+					+ "from users u, sales s, products p, categories c "
+					+ "where u.id = s.uid and s.pid = p.id and p.cid = c.id "
+					+ "group by c.id, u.state;";
 			stmt.execute(SQL);
+			
+			SQL = "insert into p_customer_product "
+					+ "select u.id, p.id, sum(s.quantity * s.price) as total "
+					+ "from users u, sales s, products p "
+					+ "where u.id = s.uid and s.pid = p.id "
+					+ "group by u.id, p.id;";
 			stmt.execute(SQL);
+			
+			SQL = "insert into p_category_product "
+					+ "select c.id as cid, p.id as pid, sum(s.price * s.quantity) as agg, p.name "
+					+ "from categories c, products p, sales s "
+					+ "where s.pid = p.id and p.cid = c.id "
+					+ "group by c.id, p.id;";
 			stmt.execute(SQL);
-			stmt.execute(SQL);
+			
+			SQL = "insert into p_category_product_state "
+					+ "select c.id as cid, p.id as pid, u.state, sum(s.price * s.quantity) as agg, p.name "
+					+ "from categories c, products p, users u, sales s "
+					+ "where s.uid = u.id and p.id = s.pid and p.cid = c.id "
+					+ "group by c.id, p.id, u.state;";
 			stmt.execute(SQL);
 			
 			
