2d7b314bd0ebd4deffae0153dc788b8549c96730
ehdee <ehdee@eddies-mbp>
2014-06-06 17:04:21 -0700

    FirstPrecomputationProgram now creates the tables too

--- src/cse135/FirstPrecomputationProgram.java
+++ src/cse135/FirstPrecomputationProgram.java
@@ -2,6 +2,7 @@ package cse135;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,6 +12,7 @@ public class FirstPrecomputationProgram {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Program started...");
 		
 		Connection conn=null;
 		Statement stmt=null;
@@ -26,7 +28,31 @@ public class FirstPrecomputationProgram {
 			
 			stmt =conn.createStatement();
 			
+			System.out.println("Creating precomputation tables...");
+			
+			PreparedStatement ps = conn.prepareStatement("CREATE TABLE p_category_customer_state (   cid integer NOT NULL,   uid integer NOT NULL,   state text NOT NULL,   agg integer NOT NULL,   uname text NOT NULL,   CONSTRAINT p_category_customer_state_pkey PRIMARY KEY (cid, uid, state) );");
+			ps.executeUpdate();
+			
+			ps = conn.prepareStatement("CREATE TABLE p_category_product (   cid integer NOT NULL,   pid integer NOT NULL,   agg integer NOT NULL,   pname text NOT NULL,   CONSTRAINT p_category_product_pkey PRIMARY KEY (cid, pid) );");
+			ps.executeUpdate();
+			
+			ps = conn.prepareStatement("CREATE TABLE p_category_product_state (   cid integer NOT NULL,   pid integer NOT NULL,   state text NOT NULL,   agg integer NOT NULL,   pname text NOT NULL,   CONSTRAINT p_category_product_state_pkey PRIMARY KEY (cid, pid, state) );");
+			ps.executeUpdate();
+			
+			ps = conn.prepareStatement("CREATE TABLE p_category_state (   cid integer NOT NULL,   state text NOT NULL,   agg integer NOT NULL,   CONSTRAINT p_category_state_pkey PRIMARY KEY (cid, state) );");
+			ps.executeUpdate();
+			
+			ps = conn.prepareStatement("CREATE TABLE p_customer_product (   uid integer NOT NULL,   pid integer NOT NULL,   agg integer NOT NULL,   CONSTRAINT p_customer_product_pkey PRIMARY KEY (uid, pid) );");
+			ps.executeUpdate();
+			
+			ps = conn.prepareStatement("CREATE TABLE p_customer_state (   uid integer NOT NULL,   state text NOT NULL,   agg integer NOT NULL,   uname text NOT NULL,   CONSTRAINT p_customer_state_pkey PRIMARY KEY (uid, state) );");
+			ps.executeUpdate();
+			
+			ps.close();
+			
 			String SQL = "";
+			
+			System.out.println("Populating precomputation tables...");
 			
 			SQL = "insert into p_category_customer_state "
 					+ "select c.id as cid, u.id as uid, u.state, sum(s.price * s.quantity) as total, u.name "
@@ -70,14 +96,16 @@ public class FirstPrecomputationProgram {
 					+ "group by c.id, p.id, u.state;";
 			stmt.execute(SQL);
 			
-			
-			
+
 			
 			conn.close();
+			
+			System.out.println("Program completed!");
 			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			System.out.println("Woops, SQLException was thrown!");
 			e.printStackTrace();
 		}
 		
